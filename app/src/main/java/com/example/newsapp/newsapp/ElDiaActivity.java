ece457711eb87a89d1bdb1d43e8dc313b0462b7d
LizzieDM <omairaidm@gmail.com>
2015-05-06 22:55:45 +0100

    Comienzo
    Comienzo del proyecto, realizo los botones de acceso a los principales
    periodicos pero no tienen contenidos

---
+++ app/src/main/java/com/example/newsapp/newsapp/ElDiaActivity.java
@@ -0,0 +1,20 @@
+package com.example.newsapp.newsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+/**
+ * Created by ASUS on 22/04/2015.
+ */
+public class ElDiaActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_el_dia);
+    }
+
+
+}
